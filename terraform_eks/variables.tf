fff011da4c59508f1f25b79d3792a5c33e3d415b
Ruhon Deb <145097266+rohandeb2@users.noreply.github.com>
2025-10-06 01:32:59 +0530

    Update variables.tf

--- terraform_eks/variables.tf
+++ terraform_eks/variables.tf
@@ -1,5 +1,7 @@
 variable "ssh_key_name" {
 	description = "The name of the SSH Key pair to use for instance"
 	type = string
-	default = "20 dollar ec2 instance"
+	default = "20 dollar ec2 instance"     # if you have an existing key pair, use the previous one, or create a new one by creating a new EC2 instance 
+											# how to check if already exists or not aws command aws ec2 describe-key-pairs --region us-east-1
+
 }
